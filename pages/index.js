3cc5840889a1a5174f1975979e46004275591393
Hedieh <heydari.hedieh2680@gmail.com>
2023-09-18 21:30:28 +0330

    add component of the home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
-import Details from './cars/index'
-export default function Home() {
+import Categories from "../components/module/Categories";
+import SearchBar from "../components/module/SearchBar";
+import CarsPage from "../components/templates/CarsPage";
+import carsData from '../data/CarsData'
+import AllButton from "../components/module/AllButton";
+
+function Home() {
+  const cars = carsData.slice(0,3)
   return (
     <div>
-    <Details/>
+      <SearchBar/>
+      <Categories/>
+      <AllButton/>
+      <CarsPage carsData={cars} />
     </div>
-  )
+  );
 }
+
+export default Home;
